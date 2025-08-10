26a12f1a6476ac23b2a5362cd34902ff845544f2
yangkevin005 <yangkevin005@gmail.com>
2025-08-09 22:12:21 -0400

    Added variables.tf and main.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "linux_instance_type" {
+  default = "t2.micro"
+}
+
+variable "windows_instance_type" {
+  default = "t2.micro"
+}
+
+variable "linux_ami" {
+  default = "ami-084a7d336e816906b"  
+}
+
+variable "windows_ami" {
+  default = "ami-0c9fb5d338f1eec43"   
+}
